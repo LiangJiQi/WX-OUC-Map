241f19c9eef091d4cda6d0f73317d00b794b3ba1
Liang MengMeng <32237967+LiangJiQi@users.noreply.github.com>
2018-06-18 18:42:22 +0800

    18/6/18 qi seven

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
 //app.js
 
-var first = 0;
-
 App({
   onLaunch: function () {
-    wx.reLaunch({
-      url: '../../pages/map/map',
-    })
+
   },
 
   onShow:function(){
-    if(this.globalData.first){
-      wx.redirectTo({
-        url: '../../pages/help/help',
-      })
-    } else {
-      wx.reLaunch({
-        url: '../../pages/map/map',
-      })
-    }
+
   },
 
   onHide:function(){
-    this.globalData.first = 1;
+
   },
 
   globalData: {
